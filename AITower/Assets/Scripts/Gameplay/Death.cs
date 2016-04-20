fee1361544a497e4712446a3f48ceff9d92c700c
jdewbank <jdewbank@gmail.com>
2016-04-20 17:05:14 -0500

    Added JoeyScene for testing and Death script

---
+++ AITower/Assets/Scripts/Gameplay/Death.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour {
+
+    public int deathCount = 0;
+    public Vector3 startPosition = new Vector3(0, 1, -5);
+
+    void Start ()
+    {
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == "death")
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        deathCount++;
+        transform.position = new Vector3(0, 1, -5);
+        Debug.Log("Death Count: " + deathCount);
+    }
+}
