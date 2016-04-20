e2f3134771d84b1163ff5ad889d3e696c59b1b6f
ZackMisso <zack441@mac.com>
2016-04-20 16:07:47 -0500

    Added Elevator Control

--- AITower/Assets/Scripts/Gameplay/Elevator/ElevatorControl.cs
+++ AITower/Assets/Scripts/Gameplay/Elevator/ElevatorControl.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class ElevatorControl : MonoBehaviour {
+	public float endHeight = 20.0f;
+	private bool escalating = false;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+		if(transform.position.y >= endHeight)
+			escalating = false;
+		if(escalating)
+			transform.position = new Vector3(transform.position.x,transform.position.y+.05f,transform.position.z);
+}
+
+	void OnTriggerEnter(Collider other) {
+		//transform.position = new Vector3(transform.position.x,transform.position.y+1,transform.position.z);
+		//Debug.Log("Triggered");
+		escalating = true;
 	}
 }
