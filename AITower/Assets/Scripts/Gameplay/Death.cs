9e6cb38c6683b8f956389a201a0c5e29bfdc5e51
jdewbank <jdewbank@gmail.com>
2016-04-20 17:51:40 -0500

    Added GUI overlay for deaths

--- AITower/Assets/Scripts/Gameplay/Death.cs
+++ AITower/Assets/Scripts/Gameplay/Death.cs
@@ -5,10 +5,11 @@ public class Death : MonoBehaviour {
 
     public int deathCount = 0;
     public Vector3 startPosition = new Vector3(0, 1, -5);
+    private GUIStyle style = new GUIStyle();
 
     void Start ()
     {
-
+        style.alignment = TextAnchor.MiddleCenter;
     }
 
 	// Update is called once per frame
@@ -29,5 +30,14 @@ public class Death : MonoBehaviour {
         deathCount++;
         transform.position = new Vector3(0, 1, -5);
         Debug.Log("Death Count: " + deathCount);
+    }
+
+    void OnGUI ()
+    {
+        string death_string = deathCount.ToString() + " death";
+        if (deathCount != 1)
+            death_string += "s";
+        
+        GUI.Box(new Rect(5, Screen.height - 30,80,25), death_string, style);
     }
 }
