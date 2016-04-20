4f0e54987fa72eb87ccbee47648badeb66adb5ac
ZackMisso <zack441@mac.com>
2016-04-20 17:44:53 -0500

    Finished Sine Trajectory

--- AITower/Assets/Scripts/Gameplay/Movement/Trajectories/SineTrajectory.cs
+++ AITower/Assets/Scripts/Gameplay/Movement/Trajectories/SineTrajectory.cs
@@ -3,18 +3,21 @@ using System.Collections;
 
 public class SineTrajectory : Trajectory {
 	public Vector3 sineMap = new Vector3(0.0f,1.0f,0.0f);
-	public float amplitude = .05f;
+	public float amplitude = 1.0f;
+	//public float length = 0.0f;
 
 	public override void FollowTrajectory() {
-		float deltaTime = Time.time - lastUpdateTime;
+		float length = Time.time - startTime;
 		//Vector3 sineMap = (new Vector3(1.0f,1.0f,1.0f)) - lineOfSight;
-    //Debug.Log(Mathf.Sin(Time.time)  * .01f);
-		Debug.Log(initialPosition.y + lineOfSight.y * frequency * deltaTime);
-    float dX = initialPosition.x + lineOfSight.x * frequency * deltaTime + sineMap.x * Mathf.Sin(Time.time) * amplitude;
-    float dY = initialPosition.y + lineOfSight.y * frequency * deltaTime + sineMap.y * Mathf.Sin(Time.time) * amplitude;
-    float dZ = initialPosition.z + lineOfSight.z * frequency * deltaTime + sineMap.z * Mathf.Sin(Time.time) * amplitude;
-    transform.position = new Vector3(transform.position.x+dX,transform.position.y+dY,transform.position.z+dZ);
-    lastUpdateTime = Time.time;
-		// to be implemented
+    //Debug.Log(Mathf.Sin(Time.time) * amplitude);
+		//Debug.Log(initialPosition.y + lineOfSight.y * frequency * deltaTime);
+		float newX = initialPosition.x + lineOfSight.x*length;
+		float newY = initialPosition.y + lineOfSight.y*length;
+		float newZ = initialPosition.z + lineOfSight.z*length;
+    float dX = sineMap.x * Mathf.Sin(Time.time) * amplitude;
+    float dY = sineMap.y * Mathf.Sin(Time.time) * amplitude;
+    float dZ = sineMap.z * Mathf.Sin(Time.time) * amplitude;
+    transform.position = new Vector3(newX+dX,newY+dY,newZ+dZ);
+    //lastUpdateTime = Time.time;
 	}
 }
