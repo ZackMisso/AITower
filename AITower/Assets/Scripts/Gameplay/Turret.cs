9e08f2000f9d95a96ec4d8ff918a75cc12b6dcea
BrianShiau <brian-s@utexas.edu>
2016-04-22 17:01:39 -0500

    added tower. bullets stopped moving for some reason

---
+++ AITower/Assets/Scripts/Gameplay/Turret.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+	public LineTrajectory lineTraj;
+	public float reloadTime = 0;
+
+	float timeSinceLastBullet;
+
+	// is just transform.position
+	//public Vector3 position;
+
+	//is just transform.right
+	//public Vector3 lookDirection;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Rotate(Vector3.up, Time.deltaTime*90);
+
+		timeSinceLastBullet += Time.deltaTime;
+		if (timeSinceLastBullet >= reloadTime) {
+			LineTrajectory lt = (LineTrajectory)Instantiate(lineTraj, transform.position, Quaternion.identity);
+			lt.lineOfSight = transform.right;
+			lt.initialPosition = transform.position;
+
+			timeSinceLastBullet = 0;
+		}
+	}
+}
