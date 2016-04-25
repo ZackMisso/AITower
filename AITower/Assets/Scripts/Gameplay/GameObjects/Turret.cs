a3b5633139b4f8900894c06aa19c97a7042d8ab6
ZackMisso <zack441@mac.com>
2016-04-25 18:09:08 -0500

    Cleaned Up Turret and Weapon Classes

--- AITower/Assets/Scripts/Gameplay/GameObjects/Turret.cs
+++ AITower/Assets/Scripts/Gameplay/GameObjects/Turret.cs
@@ -2,21 +2,7 @@
 using System.Collections;
 
 public class Turret : MonoBehaviour {
-	public Weapon weapon;
-
-
-	//public LineTrajectory lineTraj;
-	//public float reloadTime = 0;
-
-	//float timeSinceLastBullet;
-
-	// is just transform.position
-	// ^^ But we can have turrets of different sizes. Therefore we need an offset
-	// to where the cannon is for the turret
-	//public Vector3 position;
-
-	//is just transform.right
-	//public Vector3 lookDirection;
+	private Weapon weapon;
 
 	// Use this for initialization
 	void Start () {
@@ -29,15 +15,5 @@ public class Turret : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		weapon.Fire();
-		//transform.Rotate(Vector3.up, Time.deltaTime*90);
-
-		//timeSinceLastBullet += Time.deltaTime;
-		//if (timeSinceLastBullet >= reloadTime) {
-		//	LineTrajectory lt = (LineTrajectory)Instantiate(lineTraj, transform.position, Quaternion.identity);
-		//	lt.lineOfSight = transform.right;
-		//	lt.initialPosition = transform.position;
-
-		//	timeSinceLastBullet = 0;
-		//}
 	}
 }
