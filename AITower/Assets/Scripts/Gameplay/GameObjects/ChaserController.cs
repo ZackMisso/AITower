1b72848f5695816770f71daa3402caad3bcb2daa
BrianShiau <brian-s@utexas.edu>
2016-04-27 17:57:16 -0500

    reset levels on death

--- AITower/Assets/Scripts/Gameplay/GameObjects/ChaserController.cs
+++ AITower/Assets/Scripts/Gameplay/GameObjects/ChaserController.cs
@@ -5,11 +5,12 @@ public class ChaserController : MonoBehaviour
 {
 	public float speed;
 	public GameObject player; //set this to the player
+	public Vector3 startPosition;
 
 	// Use this for initialization
 	void Start ()
 	{
-	
+		transform.position = startPosition;
 	}
 	
 	// Update is called once per frame
@@ -25,6 +26,11 @@ public class ChaserController : MonoBehaviour
 		difference.y = 0;
 		Debug.Log (difference * speed);
 		transform.position = transform.position + (difference * speed);
+	}
+
+	public void Reset ()
+	{
+		transform.position = startPosition;
 	}
 }
 
