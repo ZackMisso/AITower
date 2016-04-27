7647905fcc4349198e455118299f0e0c1cdabc8d
BrianShiau <brian-s@utexas.edu>
2016-04-27 17:35:40 -0500

    added chaser, currently walks through objects

---
+++ AITower/Assets/Scripts/Gameplay/GameObjects/ChaserController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaserController : MonoBehaviour
+{
+	public float speed;
+	public GameObject player; //set this to the player
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 difference = player.transform.position - transform.position;
+		difference.Normalize ();
+		difference.y = 0;
+		Debug.Log (difference * speed);
+		transform.position = transform.position + (difference * speed);
+	}
+}
+
