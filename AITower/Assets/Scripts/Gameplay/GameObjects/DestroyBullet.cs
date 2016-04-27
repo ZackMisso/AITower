743add0d59a360f4a288bc6f7771a4ca0255a08f
ZackMisso <zack441@mac.com>
2016-04-27 16:26:40 -0500

    Finished Destroy Bullet

--- AITower/Assets/Scripts/Gameplay/GameObjects/DestroyBullet.cs
+++ AITower/Assets/Scripts/Gameplay/GameObjects/DestroyBullet.cs
@@ -6,7 +6,6 @@ public class DestroyBullet : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
     // This will need to be changed for GA. Trajectory should be updated
     if(other.gameObject.tag == "bulletDeath") {
-      
       Destroy(other.gameObject);
     }
 	}
