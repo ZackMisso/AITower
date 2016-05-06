b5312b24c9dff8cfd679ce2d80bbc2c2326e52e2
BrianShiau <brian-s@utexas.edu>
2016-05-05 20:10:05 -0500

    changed chasers to return to its starting point if the player is too far

--- AITower/Assets/Scripts/Gameplay/GameObjects/ChaserController.cs
+++ AITower/Assets/Scripts/Gameplay/GameObjects/ChaserController.cs
@@ -15,7 +15,9 @@ public class ChaserController : MonoBehaviour
 	{
 		transform.position = startPosition;
 		LineRenderer lineRenderer = GetComponent<LineRenderer> ();
-		lineRenderer.SetPosition (0, startPosition);
+		Vector3 removeY = startPosition;
+		removeY.y = 0;
+		lineRenderer.SetPosition (0, removeY);
 	}
 	
 	// Update is called once per frame
@@ -26,15 +28,15 @@ public class ChaserController : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		Vector3 difference = player.transform.position - transform.position;
-		difference.Normalize ();
-		difference.y = 0;
-		transform.position = transform.position + (difference * speed);
-		if ((transform.position - startPosition).magnitude > tetherDistance) {
-			Vector3 startDifference = player.transform.position - startPosition;
+		if ((player.transform.position - startPosition).magnitude > tetherDistance) {
+			Vector3 startDifference = startPosition - transform.position;
 			startDifference.Normalize ();
-			startDifference.y = 0;
-			transform.position = startPosition + startDifference * tetherDistance;
+			transform.position = transform.position + (startDifference * speed);
+		} else {
+			Vector3 difference = player.transform.position - transform.position;
+			difference.y = 0;
+			difference.Normalize ();
+			transform.position = transform.position + (difference * speed);
 		}
 
 		LineRenderer lineRenderer = GetComponent<LineRenderer> ();
